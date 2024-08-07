e06de159d0aaf9b99ac2105f7cee75935e0613f5
StalinVinueza <csvinueza@puce.edu.ec>
2024-08-07 18:33:47 -0500

    modelos

--- album_manager/models.py
+++ album_manager/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    last_name =models.CharField(max_length=30, null=False)
+    country = models.CharField(max_length=30, null=False)
+
+    def __str__(self) -> str:
+        return f'{self.name} {self.last_name}'
+
+class Album(models.Model):
+    title = models.CharField(max_length=30, null=False)
+    release_year = models.DateField()
+    MUSIC_GENRES ={
+        ("Rock", "Rock"),
+        ("Música Clasica", "Música Clasica"),
+        ("Cumbias", "cumbias"),
+        ("Pop", "Pop"),
+        ("Electrónica", "Electrónica")
+    }
+    genre = models.CharField(max_length=30, choices=MUSIC_GENRES, null=False) 
+    associate_artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    front_page = models.ImageField(upload_to='front_page_images')
+
+    def __str__(self) -> str:
+        return self.title
--- albums/settings.py
+++ albums/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'album_manager'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'albums.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pokedex',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'stalin',
+        'PASSWORD': 'Romi19'
     }
 }
 
@@ -117,7 +122,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+#Media Files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración de login
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = 'album_manager:login'
